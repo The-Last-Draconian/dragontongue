29083035c9d321fa212c5f14cd7f811b52916c3e
The-Last-Draconian <tay20045@byui.edu>
2022-04-20 07:19:13 -0600

    c

--- javascript/dictionary.js
+++ javascript/dictionary.js
@@ -4,30 +4,29 @@ const requestURL = "https://the-last-draconian.github.io/dragontongue/json/dicti
 
 //fetching the URL and making a Promise
 fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        console.table(jsonObject);
-        //Array function based off Virtual Lab example
-         const dictionaryData = jsonObject["dictionary"];
-        //Filter out relevant data
-        const firstLetter = dictionaryData.filter((letter) => letter.first-letter);
-         firstLetter.forEach((letter) => {
-            //first-letter div
+    .then((response) => response.json())
+    .then((dictionary) => {
+        console.log(dictionary);
+
+        let firstLetter = dictionary.firstLetter;
+        
+        for (i=0; i < 4; i++) {
             let flDiv = document.createElement("div");
             flDiv.setAttribute("class", "first-letter");
 
             //first-letter heading
             let flh2 = document.createElement("h2");
-            flh2.textContent = "testing";
-            //flh2.textContent = `${letter.first-letter}`;
-            
+            flh2.textContent = firstLetter[i];
+                
+            let testing = document.createElement("p");
+            testing.textContent = "this is a test";
+
             //ordering the content
             flDiv.appendChild(flh2);
+            flDiv.appendChild(testing);
 
             //exporting the content to the restaurants section
             document.querySelector("#dictionary-wrapper").appendChild(flDiv);
 
-        });
+        }
     });
